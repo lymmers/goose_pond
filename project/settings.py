b7cf838d230d94b84953ac7dd31589f5ab8846ba
Leon Sandøy <leon.haland@gmail.com>
2022-03-29 08:10:16 +0200

    Add www.lymmers.com to ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ else:
     DEBUG = False
 
 if not DEBUG:
-    ALLOWED_HOSTS = ["lymmers.com"]
+    ALLOWED_HOSTS = ["lymmers.com", "www.lymmers.com"]
 else:
     ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
 
