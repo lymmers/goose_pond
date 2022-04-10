5f32b54a8fcd6295b014e8baf623a07a75fa5484
Lymmers <tommas.haland@gmail.com>
2022-04-10 13:44:31 +0200

    made the pond lemoner

--- apps/goosepond/views/index.py
+++ apps/goosepond/views/index.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 GEESE = [
     {"url": "https://google.com", "flipped": True, "image": "images/pilgrim_goose.png"}, 
-    {"url": "https://yahoo.com", "flipped": False, "image": "images/embden_goose.png"}, 
+    {"url": "https://lemonsaur.us", "flipped": False, "image": "images/lemon-duck.png"}, 
     {"url": "/therapy", "flipped": True, "image": "images/therapy_goose.png"},
     {"url": "https://bing.com", "flipped": False, "image": "images/embden_goose.png"},
 ]
