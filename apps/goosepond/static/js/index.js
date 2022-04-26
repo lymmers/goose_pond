89227db72362f18e7ebef8b9ff7bea61825d909c
Lymmers <tommas.haland@gmail.com>
2022-04-26 22:55:21 +0200

    implement goosegame easter egg

--- apps/goosepond/static/js/index.js
+++ apps/goosepond/static/js/index.js
@@ -6,4 +6,18 @@ $(document).ready(function() {
         $(this).css("animation-duration", `${animationSpeed}s`)
     })
 
+    // Make goose steal cursor when you click the cattails
+    $("#reed-area").click(function() {
+        $("#goose-game-goose").removeClass("idle")
+        $("#goose-game-goose").addClass("run")
+        $("html").addClass("hide-cursor")
+        $("#reed-area").addClass("hide-cursor")
+        $("#goose-game-honk")[0].play()
+        setTimeout(function() {
+            window.location.href = "https://goose.game"
+        }, 4200);
+
+    });
+
 });
+
