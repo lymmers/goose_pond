175a1c7f19b824811013bef087c62652d4500f27
Leon Sandøy <leon.haland@gmail.com>
2022-04-27 01:59:57 +0200

    This is now provided by utils.js

--- apps/goosepond/static/js/therapy.js
+++ apps/goosepond/static/js/therapy.js
@@ -41,7 +41,6 @@ $(document).ready(function() {
     var input = $(".input");
     var readyForInput = false;
 
-    $('map').imageMapResize();
     freshGooseHonks = shuffle(freshGooseHonks);
 
     body.html(bodytext);
