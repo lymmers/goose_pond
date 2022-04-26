798e38a274bcf067f5cfacf98d71114a9ac6a413
Lymmers <tommas.haland@gmail.com>
2022-04-26 18:39:51 +0200

    Fixed HTMX CSRF token

--- apps/goosepond/views/goosename/name_component.py
+++ apps/goosepond/views/goosename/name_component.py
@@ -9,6 +9,7 @@ class NameComponent(View):
     def post(self, request):
         """Render the goosename component."""
         gooseinput = request.POST.get("gooseinput")
+        log.critical(list(request.POST.items()))
         return render(
             request,
             "components/goosename.html",
